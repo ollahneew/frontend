e1cb7e960779937408229d2ef45af51100b23d9f
ollahneew <92301392+ollahneew@users.noreply.github.com>
2024-04-09 19:33:18 +0700

    fix search

--- src/components/ButtonInputSearch/ButttonInputSearch.jsx
+++ src/components/ButtonInputSearch/ButttonInputSearch.jsx
@@ -6,7 +6,7 @@ import ButtonComponent from '../ButtonComponent/ButtonComponent'
 
 const ButttonInputSearch = (props) => {
   const {
-    size, placeholder, textButton,
+    size, placeholder, textbutton,
     bordered, backgroundColorInput = '#fff',
     backgroundColorButton = 'rgb(13, 92, 182)',
     colorButton = '#fff'
@@ -19,12 +19,13 @@ const ButttonInputSearch = (props) => {
         placeholder={placeholder}
         bordered={bordered}
         style={{ backgroundColor: backgroundColorInput }}
+        {...props}
       />
       <ButtonComponent
         size={size}
         styleButton={{ background: backgroundColorButton, border: !bordered && 'none' }}
         icon={<SearchOutlined color={colorButton} style={{ color: '#fff' }} />}
-        textButton={textButton}
+        textbutton={textbutton}
         styleTextButton={{ color: colorButton }}
       />
     </div>
